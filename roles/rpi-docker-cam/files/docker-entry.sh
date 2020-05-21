9eb5ad9178de877520527ec9d0a3e8af2411ca8b
David <david@ldkloos>
2020-05-21 15:14:52 +0200

    update

--- roles/rpi-docker-cam/files/docker-entry.sh
+++ roles/rpi-docker-cam/files/docker-entry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp RPi_Cam_Web_Interface/bin/raspimjpeg /usr/local/bin/
 chmod 755 /usr/local/bin/raspimjpeg
-
+export LD_LIBRARY_PATH=/opt/vc/lib
 sudo service nginx restart
 RPi_Cam_Web_Interface/start.sh
 
@@ -9,7 +9,8 @@ RPi_Cam_Web_Interface/start.sh
 if [ $# -eq 0 ]
 then
     echo "Running php-fpm in foreground"
-    php-fpm7.0 -F
+    /bin/bash
+    #php-fpm7.0 -F
 elif [ $1 == "debug" ]
 then
     echo "debug"
